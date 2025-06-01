ee2970e043e6c193cb92a1ecca2f902561291c69
to3aunglin <toeaunglin41@gmail.com>
2025-06-01 18:53:11 +0630

    index.html ui testing

--- src/main/java/com/example/taskmanager/HomeController.java
+++ src/main/java/com/example/taskmanager/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Welcome to Task Manager!");
+
         return "index";
     }
 }
